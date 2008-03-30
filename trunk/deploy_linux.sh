c1e998c435bca3fcdf4e9f09d613622b635fae02
bastibense <bastibense@5e5cd925-a849-0410-9d9c-19b0accd6922>
2008-03-30 14:48:37 +0000

    Some tools.

--- trunk/deploy_linux.sh
+++ trunk/deploy_linux.sh
@@ -8,6 +8,8 @@ echo "- Preparing qwired build to be deployed."
 if [ -e bin/qwired ]; then
     echo "- Packing"
     tar -zcf $DMGNAME bin/qwired INSTALL README
+    echo "- Uploading to Google"
+    python contrib/googlecode_upload.py -p "qwired-suite" -s "Qwired Client Snapshot r`svnversion`" -l "Type-Archive,OpSys-Linux,Version-Snapshot" -u bastibense $DMGNAME
 	echo "- Done"
     exit 0
 
