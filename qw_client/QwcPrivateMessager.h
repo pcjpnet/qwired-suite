118672a5ce60bb37fbcd855add24630a6c0ae71b
bastibense <bastibense@5e5cd925-a849-0410-9d9c-19b0accd6922>
2009-03-09 18:28:10 +0000

    Some fixes for the messaging

--- qw_client/QwcPrivateMessager.h
+++ qw_client/QwcPrivateMessager.h
@@ -6,23 +6,27 @@
 #include <QWidget>
 
 
+/*! The session contains the QTextDocument and user information for a specific conversation (session)
+    in the messenger. It is passed using the QVariant mechanism to provide the information for the
+    delegate.
+*/
 class QwcPrivateMessagerSession
 {
-
 public:
     QwcPrivateMessagerSession();
     int unreadCount;
     QwcUserInfo userInfo;
     QTextDocument *document;
-
 };
 Q_DECLARE_METATYPE(QwcPrivateMessagerSession);
 
 
+
+/*! This delegate is responsible for drawing the user list in the messenger window.
+*/
 class QwcPrivateMessageListDelegate : public QItemDelegate
 {
-    Q_OBJECT
-
+Q_OBJECT
 public:
     QwcPrivateMessageListDelegate(QObject *parent=0);
     void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
@@ -31,12 +35,14 @@ public:
 
 
 
+/*! This is the main messenger widget. It is responsible for the user interaction.
+*/
 class QwcPrivateMessager : public QWidget, private Ui_QwcPrivateMessager
 {
-    Q_OBJECT
-
+Q_OBJECT
 public:
     QwcPrivateMessager(QWidget *parent=0);
+    bool eventFilter(QObject *watched, QEvent *event);
 
 private:
     QGraphicsScene *graphicsScene;
@@ -48,8 +54,8 @@ private slots:
 public slots:
     void handleNewMessage(const QwcUserInfo &sender, const QString message);
 
-
-
+signals:
+    void enteredNewMessage(const int targetUserId, const QString message);
 };
 
 #endif // QWCPRIVATEMESSAGER_H
