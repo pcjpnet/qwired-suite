e7f4c5810b8938c42a05d08a354d5abdbea2157f
bastibense <bastibense@5e5cd925-a849-0410-9d9c-19b0accd6922>
2009-03-31 22:18:30 +0000

    Fix for the test-tracker implementation.

--- qw_server/main.cpp
+++ qw_server/main.cpp
@@ -1,27 +1,3 @@
-/***************************************************************************
- * QWired - a free Wired client.
- * Copyright (C) 2008 Bastian Bense
- ***************************************************************************
- * This program is free software; you can redistribute it and/or modify it *
- * under the terms of the GNU General Public License as published by the   *
- * Free Software Foundation; either version 2 of the License, or           *
- * (at your option) any later version.                                     *
- *                                                                         *
- * This program is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of              *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU        *
- * General Public License for more details.                                *
- *                                                                         *
- * You should have received a copy of the GNU General Public License along *
- * with this program; if not, write to the                                 *
- * Free Software Foundation, Inc.,                                         *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA                   *
- ***************************************************************************/
-
-/* This is the source code of the Qwired server. It is basically a Wired 1.1 server that also
-   implements some nice extensions.
-*/
-
 #include <QtCore>
 #include "QwsServerController.h"
 #include "QwTrackerClientSocket.h"
@@ -32,16 +8,20 @@ int main (int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
 
-    QwTrackerClientSocket *trackerSocket = new QwTrackerClientSocket(0);
-    trackerSocket->connectToTracker("wired.zankasoftware.com");
+//    QwTrackerClientSocket *trackerSocket = new QwTrackerClientSocket(0);
+//    trackerSocket->mode = Qw::TrackerClientSocketModeAutomatic;
+//    trackerSocket->localServerInfo.name = "Test Servar";
+//    trackerSocket->localServerInfo.description = "No Comment";
+//    trackerSocket->localServerInfo.url = "wired://qwired.neo.de/";
+//    trackerSocket->connectToTracker("wired.zankasoftware.com");
 
-//    QwsServerController controller;
-//    if (!controller.loadConfiguration()) {
-//        return 1;
-//    }
-//    if (!controller.startServer()) {
-//        return 1;
-//    }
+    QwsServerController controller;
+    if (!controller.loadConfiguration()) {
+        return 1;
+    }
+    if (!controller.startServer()) {
+        return 1;
+    }
 
 
 
