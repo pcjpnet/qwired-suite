25a9bd2c51c1923f605ad0bddbec40559c106de0
bastibense <bastibense@5e5cd925-a849-0410-9d9c-19b0accd6922>
2009-06-20 13:08:38 +0000

    Implemented localization support to Qwired Server GUI.

--- trunk/qw_server_gui/lang/lang_de.ts
+++ trunk/qw_server_gui/lang/lang_de.ts
@@ -2,119 +2,271 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE">
 <context>
+    <name>QwmConsoleSocket</name>
+    <message>
+        <location filename="../QwmConsoleSocket.cpp" line="45"/>
+        <source>Connected to remote console.</source>
+        <translation>Mit Konsole verbunden.</translation>
+    </message>
+</context>
+<context>
+    <name>QwmMonitorController</name>
+    <message>
+        <location filename="../QwmMonitorController.cpp" line="80"/>
+        <source>Stop Server</source>
+        <translation>Server anhalten</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorController.cpp" line="81"/>
+        <source>Are you sure you want to shut down the server?</source>
+        <translation>Sind Sie sicher, dass der Serverdienst angehalten werden soll?</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorController.cpp" line="103"/>
+        <source>Server daemon terminated with a status of %1.</source>
+        <translation>Serverdienst mit einem Status von %1 angehalten.</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorController.cpp" line="135"/>
+        <source>Connecting to remote console on port %1...</source>
+        <translation>Mit Konsole verbinden an Port %1...</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorController.cpp" line="160"/>
+        <source>Transfers: %1, Download: %2/s, Upload: %3/s</source>
+        <translation>Übertragungen: %1, Downloads: %2/s, Uploads: %3/s</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorController.cpp" line="176"/>
+        <source>%1
+User: %2, Speed: %3/s</source>
+        <translation>%1
+Benutzer: %2, Geschwindigkeit: %3/s</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorController.cpp" line="214"/>
+        <source>%1h %2m</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
     <name>QwmMonitorWindow</name>
     <message>
         <location filename="../QwmMonitorWindow.ui" line="14"/>
         <source>Qwired Server</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="28"/>
+        <location filename="../QwmMonitorWindow.ui" line="38"/>
         <source>Server Status</source>
         <translation>Serverstatus</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="41"/>
+        <location filename="../QwmMonitorWindow.ui" line="155"/>
+        <source>Rebuild File Database</source>
+        <translation>Dateiindex neu aufbauen</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="184"/>
+        <source>Generate a new private key and certificate for server/client encryption.</source>
+        <translation>Ein neues Zertifikat-/Schlüsselpärchen für die Client/Server-Verschlüsselung erzeugen.</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="187"/>
+        <source>Regenerate Certificate</source>
+        <translation>Zertifikat Erneuern</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="216"/>
+        <location filename="../QwmMonitorWindow.ui" line="248"/>
+        <source>Set a new password for the &apos;admin&apos; account.</source>
+        <translation>Neues Passwort für das &apos;admin&apos;-Konto setzen.</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="219"/>
+        <source>Set Admin Password</source>
+        <translation>Admin-Passwort setzen</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="251"/>
+        <source>Purge News</source>
+        <translation>Neuigkeiten löschen</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="280"/>
         <source>Statistics</source>
         <translation>Statistiken</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="55"/>
+        <location filename="../QwmMonitorWindow.ui" line="288"/>
         <source>Connected Users:</source>
         <translation>Verbundene Benutzer:</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="62"/>
-        <location filename="../QwmMonitorWindow.ui" line="82"/>
-        <location filename="../QwmMonitorWindow.ui" line="102"/>
-        <location filename="../QwmMonitorWindow.ui" line="109"/>
-        <source>n/a</source>
-        <translation>n/v</translation>
+        <location filename="../QwmMonitorWindow.ui" line="575"/>
+        <source>Abort Transfer</source>
+        <translation>Übertragung abbrechen</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="75"/>
+        <location filename="../QwmMonitorWindow.ui" line="602"/>
+        <source>Users</source>
+        <translation>Benutzer</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="621"/>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="626"/>
+        <source>Nickname</source>
+        <translation>Nickname</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="631"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="636"/>
+        <source>IP-Address</source>
+        <translation>IP-Addresse</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="641"/>
+        <source>Idle Time</source>
+        <translation>Untätig</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="646"/>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="675"/>
+        <source>Disconnect User</source>
+        <translation>Benutzer trennen</translation>
+    </message>
+    <message>
+        <source>n/a</source>
+        <translation type="obsolete">n/v</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="311"/>
         <source>Active Transfers:</source>
         <translation>Aktive Übertragungen:</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="95"/>
+        <location filename="../QwmMonitorWindow.ui" line="334"/>
         <source>Total Downloaded:</source>
         <translation>Heruntergeladen:</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="122"/>
+        <location filename="../QwmMonitorWindow.ui" line="370"/>
         <source>Total Uploaded:</source>
         <translation>Hochgeladen:</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="147"/>
+        <location filename="../QwmMonitorWindow.ui" line="73"/>
         <source>Controls</source>
         <translation>Steuerung</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="159"/>
+        <location filename="../QwmMonitorWindow.ui" line="88"/>
         <source>Start the server daemon on the local system.</source>
         <translation>Starte den Serverdienst auf dem lokalen System.</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="162"/>
+        <location filename="../QwmMonitorWindow.ui" line="91"/>
         <source>Start Server</source>
         <translation>Server Starten</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="185"/>
+        <location filename="../QwmMonitorWindow.ui" line="120"/>
         <source>Stop the server daemon on the local system.</source>
         <translation>Stoppe den Serverdienst auf dem lokalen System.</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="188"/>
+        <location filename="../QwmMonitorWindow.ui" line="123"/>
         <source>Stop Server</source>
         <translation>Server Stoppen</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="221"/>
+        <location filename="../QwmMonitorWindow.ui" line="152"/>
         <source>Purge and rebuild file index database.</source>
         <translation>Dateidatenbank leeren und neu aufbauen.</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="224"/>
         <source>Rebuild File Index</source>
-        <translation>Dateidatenbank Erneuern</translation>
+        <translation type="obsolete">Dateidatenbank Erneuern</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="254"/>
+        <location filename="../QwmMonitorWindow.ui" line="53"/>
         <source>Server Log</source>
         <translation>Serverprotokoll</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="271"/>
+        <location filename="../QwmMonitorWindow.ui" line="405"/>
+        <source>Configuration</source>
+        <translation>Konfiguration</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="411"/>
+        <source>Expert Settings</source>
+        <translation>Erweiterte Konfiguration</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="417"/>
+        <source>TCP Port:</source>
+        <translation>TCP-Port:</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="433"/>
+        <source>The default port is 2000, leave empty for default. File transfer connections are using this port +1. Remote console connections are made through the same port +2.</source>
+        <translation>Der Standardport ist 2000 (Feld leer lassen um Standardport zu verwenden). Dateiübertragungen finden auf diesem Port +1 statt. Konsolenverbindungen zum Serverdienst finden auf diesem Port +2 statt.</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="472"/>
+        <source>Server Banner</source>
+        <translation>Serverbanner</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="501"/>
+        <source>(not available yet)</source>
+        <translation>(noch nicht verfügbar)</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="511"/>
+        <source>Load Banner...</source>
+        <translation>Banner laden...</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="524"/>
+        <source>The server banner is displayed on all clients and should be a 200x35 pixel image.</source>
+        <translation>Der Serverbanner wird auf allen fähigen Clients dargestellt und sollte 200x35 Pixel groß sein.</translation>
+    </message>
+    <message>
+        <location filename="../QwmMonitorWindow.ui" line="542"/>
         <source>Transfers</source>
         <translation>Übertragungen</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="284"/>
-        <source>Transfers: 15, Download: 0 KB/s, Upload: 0 KB/s</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../QwmMonitorWindow.ui" line="300"/>
+        <location filename="../QwmMonitorWindow.ui" line="572"/>
         <source>Abort the selected file transfer.</source>
         <translation>Ausgewählte Übertragung abbrechen.</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="303"/>
         <source>Abort</source>
-        <translation>Abbrechen</translation>
+        <translation type="obsolete">Abbrechen</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="341"/>
+        <location filename="../QwmMonitorWindow.ui" line="672"/>
         <source>Disconnect the selected user.</source>
         <translation>Ausgewählten Benutzer vom Server trennen.</translation>
     </message>
     <message>
-        <location filename="../QwmMonitorWindow.ui" line="344"/>
         <source>Disconnect</source>
-        <translation>Trennen</translation>
+        <translation type="obsolete">Trennen</translation>
     </message>
 </context>
 </TS>
